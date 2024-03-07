bbdcc833916ef09755550de10fa0d63e14fb2242
sokny2023 <psnpsn667788@gmail.com>
2024-03-07 14:03:06 +0700

    Version 2.0 updated structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,16 @@
 
-import Footers from "./components/footer/Footer"
-import Navbars from "./components/header/Navbar"
+import Footer from "./components/footer/Footer"
 import Main from "./components/main/Main"
+import Header from "./components/header/Navbar"
 
 
 function App() {
   
-
   return (
     <>
-
-      <header className="w-full pb-4 mt-4 sticky top-0 my-6 bg-slate-50 border-b1 border-purple-50">
-          <Navbars></Navbars>
-      </header>
-
-      
+      <Header></Header>
       <Main></Main>
-
-      <footer className="w-[95%] md:w-[90%] mx-auto mt-4">
-        <Footers></Footers>
-      </footer>
-      
-
+      <Footer></Footer>
     </>
   )
 }
